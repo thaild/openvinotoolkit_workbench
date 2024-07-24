32b69b96e3d2fb40526b27aa7a6edc04e1be7c0b
Thai Le Dinh <thaild@rikkeisoft.com>
2024-07-24 10:57:44 +0700

    update requirements ubuntu

--- automation/bootstrap/install_python_packages.sh
+++ automation/bootstrap/install_python_packages.sh
@@ -27,38 +27,17 @@ if [[ ! -d ${WHEELS_FOLDER} ]]; then
 fi
 
 PYTHON_VERSION=$(python3 -c 'import sys; print(sys.version_info.major, sys.version_info.minor, sep="")')
-OPENVINO_WHEEL=$(find ${WHEELS_FOLDER} -name "openvino-202*cp${PYTHON_VERSION}*macos*.whl" -print -quit)
+OPENVINO_WHEEL=$(find ${WHEELS_FOLDER} -name "openvino-202*cp${PYTHON_VERSION}*.whl" -print -quit)
 OPENVINO_DEV_WHEEL=$(find ${WHEELS_FOLDER} -name "openvino_dev*.whl" -print -quit)
 
 python3 -m pip install ${OPENVINO_WHEEL}
-
-# export OPENBLAS=$(/opt/homebrew/bin/brew --prefix openblas)
-# export CFLAGS="-falign-functions=8 ${CFLAGS}"
-
-# # Tell scipy-1.7.3.tar.gz installer where to find lapack
-# export SYSTEM_VERSION_COMPAT=1
-# export LDFLAGS="-L/opt/homebrew/opt/lapack/lib"
-# export CPPFLAGS="-I/opt/homebrew/opt/lapack/include"
-# export PKG_CONFIG_PATH="opt/homebrew/opt/lapack/lib/pkgconfig"
-
-# # See https://github.com/scipy/scipy/issues/12935
-# export CFLAGS=-Wno-error=implicit-function-declaration
-
-# # The location may vary - use find command to find this on your local /usr/local/opt
-# export LAPACK=/opt/homebrew/opt/lapack/lib/liblapack.dylib
-# export BLAS=/opt/homebrew/opt/openblas/lib/libopenblasp-r0.3.27.dylib
-
 python3 -m pip install "${OPENVINO_DEV_WHEEL}"
-# export OPENVINO_WORKBENCH_ROOT=$(pwd)
 
-# fixed error incompatible architecture (have 'x86_64', need 'arm64e' or 'arm64')
-arch -arm64 python -m pip install numpy==1.22 pandas==1.3.5 --no-cache
-arch -arm64 python -m pip install PyNaCl==1.5.0 --no-cache
-arch -arm64 python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/requirements/requirements.txt
-arch -arm64 python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/requirements/requirements_dev.txt
-arch -arm64 python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/requirements/requirements_jupyter.txt
-arch -arm64 python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/client/automation/requirements_dev.txt
-arch -arm64 python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/model_analyzer/requirements.txt
+python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/requirements/requirements.txt
+python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/requirements/requirements_dev.txt
+python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/requirements/requirements_jupyter.txt
+python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/client/automation/requirements_dev.txt
+python3 -m pip install -r ${OPENVINO_WORKBENCH_ROOT}/model_analyzer/requirements.txt
 deactivate
 
 printf "\n Installing dependencies of deep learning frameworks \n\n"
